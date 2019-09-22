a5edffb8dd0af077a74d27bd00fe9b8a691021d6
hermoz <herminiamozomoreno@gmail.com>
2019-09-22 20:34:52 +0200

    Escribimos fichero binario e incluimos try/catch para controlar los flujos

--- nom/src/main/java/Laboral/CalculaNominas.java
+++ nom/src/main/java/Laboral/CalculaNominas.java
@@ -1,7 +1,12 @@
 package Laboral;
 
 import java.io.BufferedWriter;
+import java.io.EOFException;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileWriter;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
 
 public class CalculaNominas {
 
@@ -13,6 +18,7 @@ public class CalculaNominas {
 		 */
 		Empleado emp = new Empleado("James Cosling","32000032G",'M',4,7);
 		Empleado emp2 = new Empleado("Ada Lovelace","32000032G",'F');
+		ObjectOutputStream out = null;
 
 		//Llamamos al método escribe declarado en esta misma clase
 		escribe(emp,emp2);
@@ -25,28 +31,69 @@ public class CalculaNominas {
 		
 		//Imprimimos de nuevo para observar cambios
 		escribe(emp,emp2);
+			
+		try {
+			
+			/*
+			 * Definimos fichero llamado empleados.txt con los datos del enunciado para ambos empleados 
+			 * Realizamos la escritura en un fichero empleados.txt actualizando los datos establecidos en el 
+			 * apartado anterior
+			 * 
+			 * 	
+			 * Declaramos un nuevo método en las clases llamado: escribeFichero
+			 * para la escritura en el nuevo fichero con un formato distinto donde sólo aparezcan los valores
+			 * separados por comas
+			 */
+			
+			FileWriter escribir = new FileWriter("empleados.txt");
+			BufferedWriter bw = new BufferedWriter(escribir);
+			
+			bw.write(emp.escribeFichero()+ "\n");
+			bw.write(emp2.escribeFichero()+ "\n");
+			
+			bw.close();
+
+	
+			} catch (EOFException eof){
+				System.out.println("error en fichero");			
+			} catch (FileNotFoundException fnf) {
+				System.out.println("archivo no encontrado");
+			}
 		
+	
+		
+	try { 
 		
 		/*
-		 * Definimos fichero llamado empleados.txt con los datos del enunciado para ambos empleados 
-		 * Realizamos la escritura en un fichero empleados.txt actualizando los datos establecidos en el 
-		 * apartado anterior
-		 * 
-		 * 	
-		 * Declaramos un nuevo método en las clases llamado: escribeFichero
-		 * para la escritura en el nuevo fichero con un formato distinto donde sólo aparezcan los valores
-		 * separados por comas
+		 * Definimos fichero binario de salida con informacion solicitada
+		 * En ficheros binarios prescindimos de close y usamos finally para cerrar el flujo
 		 */
 		
-		FileWriter escribir = new FileWriter("empleados.txt");
-		BufferedWriter bw = new BufferedWriter(escribir);
 		
-		bw.write(emp.escribeFichero()+ "\n");
-		bw.write(emp2.escribeFichero()+ "\n");
+		out= new ObjectOutputStream(new FileOutputStream("sueldos.dat"));
 		
-		bw.close();
-		
-	}
+		out.writeUTF("Dni: " + emp.dni + ". Sueldo: " + Nomina.sueldo(emp) + "\n");
+		out.writeUTF("Dni: " + emp2.dni + ". Sueldo: " + Nomina.sueldo(emp2) + "\n");
+
+		//Input/Output Exception
+		} catch (IOException e) {
+			//este mensaje describe la excepcion
+			System.out.println(e.getMessage()); 
+			
+		} finally {
+			try {
+				if(out != null) {
+					out.close();
+				}
+				
+			} catch (IOException e) {
+				//este mensaje describe la excepcion
+				System.out.println(e.getMessage());	
+			}
+	
+		}
+					
+	}	
 	
 	/*
 	 * Declare un método privado escribe que reciba los valores de los dos empleados 
